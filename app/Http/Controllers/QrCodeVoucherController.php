03558561111240f11b64c6b18a6e4b84e8cb663b
arrput <ari21putra@gmail.com>
2023-11-24 08:52:56 +0700

    Generate sementara

--- app/Http/Controllers/QrCodeVoucherController.php
+++ app/Http/Controllers/QrCodeVoucherController.php
@@ -146,7 +146,7 @@ class QrCodeVoucherController extends Controller
     }
 
     public function getUserQR($id){
-        $skrg = Carbon::now()->addDays(3);
+        $skrg = Carbon::now()->addDays(1);
         $data = QrCodeVoucherModel::select('guest_list.id','guest_list.name','qrcode_voucher.code','qrcode_voucher.expired_date','qrcode_voucher.created_at')
                 ->join('guest_list','qrcode_voucher.id_guest_list','guest_list.id')
                 ->where('guest_list.id', $id)
@@ -276,7 +276,7 @@ class QrCodeVoucherController extends Controller
         $bulan_tahun = Carbon::now()->addMonth(1)->format('Y-m');
         $tgl_exp = $bulan_tahun."-05";
         $cek = QrCodeVoucherModel::select('created_at')
-                ->whereDate('created_at',Carbon::now()->addDays(3))
+                ->whereDate('created_at',Carbon::now()->addDays(1))
                 ->where('id_guest_list', $id)
                 ->first();
         // dd($cek);
@@ -285,7 +285,7 @@ class QrCodeVoucherController extends Controller
             //         ->where('id_guest_list',$id)
             //         ->first();
             
-               $skrg = Carbon::now()->addDays(3);
+               $skrg = Carbon::now()->addDays(1);
                 for($j = 0; $j < 7; $j++){
                     for($i =0; $i < 3; $i++){
                         $data = QrCodeVoucherModel::create(
